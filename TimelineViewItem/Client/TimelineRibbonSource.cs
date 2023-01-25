72f2e30c87484e4d503e601f4df2bff2e2c4509d
Ben Torkington <prmera@gmail.com>
2023-01-26 10:00:48 +1300

    add a ribbon length property to the manager and timelinesource

--- TimelineViewItem/Client/TimelineRibbonSource.cs
+++ TimelineViewItem/Client/TimelineRibbonSource.cs
@@ -1,5 +1,6 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Threading;
 using VideoOS.Platform.Client;
@@ -10,9 +11,10 @@ namespace TimelineViewItem.Client
     public class TimelineRibbonSource : TimelineSequenceSource
     {
         private Guid _sourceId;
-
-        public TimelineRibbonSource()
+        private double _ribbonLengthInSeconds = 10;
+        public TimelineRibbonSource(double ribbonLengthInSeconds)
         {
+            _ribbonLengthInSeconds = ribbonLengthInSeconds;
             _sourceId = Guid.NewGuid();
         }
 
@@ -66,18 +68,19 @@ namespace TimelineViewItem.Client
             var nextBeginTime = new DateTime(interval.StartTime.Year, interval.StartTime.Month, interval.StartTime.Day, interval.StartTime.Hour, interval.StartTime.Minute, 0, DateTimeKind.Utc);
             if (nextBeginTime < interval.StartTime)
                 nextBeginTime = nextBeginTime.AddMinutes(1);
-            var nextEndTime = nextBeginTime.AddSeconds(10);
+            var nextEndTime = nextBeginTime.AddSeconds(_ribbonLengthInSeconds);
             while (nextEndTime < interval.EndTime)
             {
                 result.Add(new TimelineDataArea(new TimeInterval(nextBeginTime, nextEndTime)));
                 nextBeginTime = nextBeginTime.AddMinutes(1);
-                nextEndTime = nextBeginTime.AddSeconds(10);
+                nextEndTime = nextBeginTime.AddSeconds(_ribbonLengthInSeconds);
             }
             return result;
         }
 
         public override void StartGetSequences(IEnumerable<TimeInterval> intervals)
         {
+            Debug.WriteLine($"in StartGetSequences() with ribbon length of {_ribbonLengthInSeconds} seconds");
             var t = new Thread(GetSomeData);
             t.Name = "Timeline sample data thread";
             t.Start(intervals);
--- TimelineViewItem/Client/TimelineViewItemViewItemManager.cs
+++ TimelineViewItem/Client/TimelineViewItemViewItemManager.cs
@@ -1,5 +1,9 @@
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using VideoOS.Platform;
 using VideoOS.Platform.Client;
+using VideoOS.Platform.Messaging;
 
 namespace TimelineViewItem.Client
 {
@@ -11,7 +15,23 @@ namespace TimelineViewItem.Client
     /// </summary>
     public class TimelineViewItemViewItemManager : ViewItemManager
     {
-        private List<TimelineSequenceSource> _timelineSequenceSources = new List<TimelineSequenceSource> { new TimelineRibbonSource(), new TimelineMarkerSource() };
+        private List<TimelineSequenceSource> _timelineSequenceSources = new List<TimelineSequenceSource> { new TimelineRibbonSource(10), new TimelineMarkerSource() };
+
+        private double _ribbonLengthInSeconds = 10;
+
+        public double RibbonLengthInSeconds { set
+            {
+                _ribbonLengthInSeconds = value;
+                // recreate the _timelineSequenceSources list with a freshly initialized sources with the new parameters
+                _timelineSequenceSources = new List<TimelineSequenceSource>
+                {
+                    new TimelineRibbonSource(_ribbonLengthInSeconds),
+                    new TimelineMarkerSource(),
+                };
+                EnvironmentManager.Instance.PostMessage(new Message(MessageId.SmartClient.RefreshTimelineRequest, null));
+                Debug.WriteLine($"set ribbon length to {value} seconds and sent RefreshTimelineRequest");
+            }
+        }
 
         public TimelineViewItemViewItemManager() : base("TimelineViewItemViewItemManager")
         {
@@ -31,6 +51,7 @@ namespace TimelineViewItem.Client
         {
             get
             {
+                Debug.WriteLine("SmartClient requested list of TimelineSequenceSources");
                 return _timelineSequenceSources;
             }
         }
